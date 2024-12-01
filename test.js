209a33b09d3566e5258946ba75dbad96927b04fa
barun-adhikari <adhikari.barun.13@gmail.com>
2024-12-01 20:38:09 +0545

    Changed: changes in the terminal message

--- test.js
+++ test.js
@@ -5,12 +5,17 @@ const launchWebDriver = require('./utils/webBrowser');
     const browserName = process.env.BROWSER;
     const baseUrl = process.env.URL;
 
+    console.log(`The test has started.`);
     const driver = await launchWebDriver(browserName);
 
     try {
+        console.log(`The browser ${browserName} has been opened for testing purpose.`);
         driver.manage().window().maximize();
+        console.log("The browser is maximized for the test.")
         await driver.get(baseUrl);
-        console.log(`The test is running.`);
+        console.log(`The Url is set and the page is loading.`);
+
+        
     } finally {
         await driver.quit();
     }
